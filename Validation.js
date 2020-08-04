5aaeb054e76cd81d94b3d0b55860fa780843580b
dulce-m-g-18 <69179333+dulce-m-g-18@users.noreply.github.com>
2020-08-03 23:26:28 -0300

    Introducción a la Programación
    Terminado e Curso.

---
+++ Validation.js
@@ -0,0 +1,20 @@
+const form = document.querySelector("#Info")
+const email = document.querySelector("#email")
+const emailHepl = document.querySelector("#emailHelp")
+const telefono =document.querySelector("#telefono")
+
+
+form.addEventistener("summit",  {event.preventDefault()
+
+if(email.value ==="" telefono.value === "" ) {
+if(email.value === "") ¨{
+emailHelp.innerText = "Por Favor Completa esta Información"
+}
+if(telefono.value=== ""){
+telefonoHelp.innerText = "Por Favor completa esta Información"
+
+}
+
+
+}
+}
